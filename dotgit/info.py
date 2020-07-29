07b95cdd8d11a5d81a655989415a80879e91c420
Kobus van Schoor <v.schoor.kobus@gmail.com>
2020-07-29 23:37:09 +0200

    bumped versions

--- dotgit/info.py
+++ dotgit/info.py
@@ -1,7 +1,7 @@
 from os.path import expanduser
 import socket
 
-__version__ = '2.0.0a7'
+__version__ = '2.0.0a8'
 __author__ = 'Kobus van Schoor'
 __author_email__ = 'v.schoor.kobus@gmail.com'
 __url__ = 'https://github.com/kobus-v-schoor/dotgit'
